6dfbb0a5b3633c53da1173c8c8e9d477e18bfe49
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-04-01 09:30:21 -0500

    se crea proyecto

---
+++ controlador.js
@@ -0,0 +1,7 @@
+let cajausuario=document.getElementByid("usuario")
+let cajacorreo=document.getElementByid("correo")
+let cajacontraseña=document.getElementByid("contraseña")
+
+//calidando el formulario
+let boton=document.getElementByid("boto")
+boton.addEventlistner("click")
